390691483259a778c0f42c0268414dbe3342f7a8
shaikmohammed9009 <72823661+shaikmohammed9009@users.noreply.github.com>
2022-08-09 02:23:52 +0530

    shaik
    shaik

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
+
+
+
 let menu =document.querySelector("#menu")
 
 let Nav = document.querySelector("ul")
@@ -7,14 +10,29 @@ menu.onclick=()=>{
     Nav.classList.toggle("active")
 }
 
+
+
 window.onscroll=()=>{
     menu.classList.remove("fa-xmark")
     Nav.classList.remove("active")
 }
+let btn=document.querySelector(".btn")
+let home=document.querySelector(".home")
+let clip = document.querySelector(".clip")
+let remove_all = document.querySelector(".rmdata")
+btn.onclick=function(){
+    btn.classList.add("active_btn")
+    clip.classList.add("active_btn")
+    home.classList.add("active_home")
+}
+remove_all.onclick=()=>{
+    btn.classList.remove("active_btn")
+    clip.classList.remove("active_btn")
+    home.classList.remove("active_home")
+}
 
-// let btn=document.querySelector(".play")
-// btn.onclick=()=>{
-//     btn.classList.add("active_btn")
-// }
+
+
+
 
 
